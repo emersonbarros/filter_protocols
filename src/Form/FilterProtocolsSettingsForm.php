4aae7e699a35f2c5e1e96d061010230ae5a34846
emersonbarros@gmail.com <emersonbarros@gmail.com>
2014-06-26 20:50:40 -0300

    Form Validate

--- src/Form/FilterProtocolsSettingsForm.php
+++ src/Form/FilterProtocolsSettingsForm.php
@@ -7,21 +7,24 @@ use Drupal\Core\Form\ConfigFormBase;
 class FilterProtocolsSettingsForm extends ConfigFormBase {
 
     public function getFormId() {
-      return 'filter_protocols_admin_settings';
+        return 'filter_protocols_admin_settings';
     }
 
     public function buildForm(array $form, array &$form_state) {
+
+        $protocols_config = $this->config('system.filter');
 
         $form['protocol'] = array(
             '#type' => 'fieldset',
             '#title' => t('Allowed Protocols'),
         );
+
         $form['protocol']['protocols'] = array(
             '#type' => 'textfield',
             '#title' => t('Allowed Protocols'),
-            '#default_value' => implode(' ',  array('ftp', 'http', 'https', 'irc', 'mailto', 'news', 'nntp', 'rtsp', 'sftp', 'ssh', 'tel', 'telnet', 'webcal')),
+            //'#default_value' => implode(' ', array('ftp', 'http', 'https', 'irc', 'mailto', 'news', 'nntp', 'rtsp', 'sftp', 'ssh', 'tel', 'telnet', 'webcal')),
             '#size' => 80,
-
+            '#value' => implode(' ', $protocols_config->get('protocols')),
         );
 
 
@@ -29,11 +32,25 @@ class FilterProtocolsSettingsForm extends ConfigFormBase {
     }
 
     public function submitForm(array &$form, array &$form_state) {
-      $protocols_config = $this->config('system.filter');
-      $protocols_config->set('protocols', $form_state['values']['protocols']);
-
-      $protocols_config->save();
+        $protocols_config = $this->config('system.filter');
+        $protocols_config->set('protocols', $form_state['values']['protocols']);
+ 
+        $protocols_config->save();
         parent::submitForm($form, $form_state);
+    }
+
+    public function validateForm(array &$form, array &$form_state) {
+        $protocols = &$form_state['values']['protocols'];
+        $protocols = drupal_strtolower(trim($protocols));
+        $protocols = preg_split('/[\s]+/', $protocols, 0, PREG_SPLIT_NO_EMPTY);
+        $protocols = array_unique($protocols);
+
+        foreach ($protocols as $protocol) {
+            if (!preg_match('/^[a-z][\w]*$/', $protocol)) {
+                $this->setFormError('', $form_state, t('Invalid protocol %protocol.', array('%protocol' => $protocol)));
+            }
+        }
+        parent::validateForm($form, $form_state);
     }
 
 }
