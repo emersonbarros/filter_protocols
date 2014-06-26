fc4b41d10b2a760c0a19891515d5091b7a825998
Thomas Bryan <thobryan@gmail.com>
2014-06-26 19:27:02 -0300

    Dojo code

--- src/Form/FilterProtocolsSettingsForm.php
+++ src/Form/FilterProtocolsSettingsForm.php
@@ -7,14 +7,11 @@ use Drupal\Core\Form\ConfigFormBase;
 class FilterProtocolsSettingsForm extends ConfigFormBase {
 
     public function getFormId() {
-        return 'filter_protocols_admin_settings';
+      return 'filter_protocols_admin_settings';
     }
 
     public function buildForm(array $form, array &$form_state) {
-      $protocols_config = $this->config('system.filter');
-      $protocols_config->set('protocols', implode(' ',  array('ftp', 'http', 'https', 'irc', 'mailto', 'news', 'nntp', 'rtsp', 'sftp', 'ssh', 'tel', 'telnet', 'webcal')));
 
-      $protocols_config->save();
         $form['protocol'] = array(
             '#type' => 'fieldset',
             '#title' => t('Allowed Protocols'),
